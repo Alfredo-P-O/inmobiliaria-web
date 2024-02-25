a441f2592faa213b802e1e8698e335473930390c
Alfredo-P-O <luisosorio38899@gmail.com>
2024-02-24 22:58:43 -0800

    Barra Vertigal Lateral - Lista

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Nos ayuda a cambiar la transparencia de la barra de navegacion horizontal
     $(window).on("scroll", function() {
         let posicion = $('#pr').offset();
         if ($(window).scrollTop() >= posicion.top) {
@@ -6,5 +7,10 @@ $(document).ready(function() {
         } else{
             $("header").addClass("transparente");
         }
+    })
+    // Metodo que asigna una clases a cierto elementos del HTML
+    $('#burger').click(function() {
+        $('#burger').toggleClass('abierto');
+        $('.menu').toggleClass('abierto');
     })
 })
